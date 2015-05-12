2b1371bcbdbadffc7167ce5f85aaa257de6cc154
Walter Gandarella <walter.wgbn@gmail.com>
2015-05-12 16:32:17 -0300

    react

---
+++ react-build/questao-07-react.js
@@ -0,0 +1,110 @@
+var meuApp = {
+    _lista: [], // lista de tarefas
+    _btnInserir: null, // botao de inserir
+
+    init: function(){
+        // adiciona evento ao botao de inserir
+        meuApp._btnInserir = document.querySelector('.barra button').addEventListener('click', meuApp.inserirTarefa);
+
+        // verifica se há uma lista na memoria
+        if (localStorage.getItem('minhaLista') != null){
+            // pega esta lista
+            meuApp._lista = JSON.parse(localStorage.getItem('minhaLista'));
+        }
+
+        // exibe a lista
+        meuApp.printTarefas();
+    },
+
+    printTarefas: function(){
+        // pega o elemento que contará a lista
+        var _tagLista = document.querySelector('.listar');
+
+        var Lista = React.createClass({displayName: "Lista",
+            getInitialState: function() {
+                return {lista: meuApp._lista};
+            },
+            render: function () {
+                var asTarefas = this.state.lista.map(function (_val, _key) {
+                    return React.createElement(ItemLista, {chave: _key, tarefa: _val.tarefa, status: _val.status});
+                });
+
+                return (
+                    React.createElement("div", {className: "lista"}, asTarefas)
+                );
+            }
+        });
+
+        var ItemLista = React.createClass({displayName: "ItemLista",
+                getInitialState: function() {
+                    return {status: this.props.status || 0};
+                },
+                toggle: function () {
+                    this.setState({status: !this.state.status});
+                },
+                render: function () {
+                    var _status = (this.state.status ? 'fundo-verde':'fundo-vermelho');
+                    return (
+                        React.createElement("div", {className: "item", id: "tarefa-"+this.props.chave, "data-id": this.props.chave, onClick: meuApp.marcarTarefa},
+                            React.createElement("div", {className: "status "+_status, id: "status-"+this.props.chave, "data-id": this.props.chave}, " "),
+                            React.createElement("div", {className: "texto ", id: "text-"+this.props.chave, "data-id": this.props.chave}, this.props.tarefa),
+                            React.createElement("div", {className: "del", id: "del-"+this.props.chave, "data-id": this.props.chave}, "X")
+                        )
+                    );
+                }
+        });
+
+        React.render(React.createElement(Lista, null), _tagLista);
+    },
+
+    inserirTarefa: function(e){
+        // pega o elemento textinpu de inserção de tarefa
+        var _txt = document.getElementById('tarefa');
+
+        // testa se foi preenchido
+        if (_txt.value.length > 0){
+            // incrementa a lista de tarefas
+            meuApp._lista.push({tarefa: _txt.value, status: 0});
+
+            // salva na memória
+            localStorage.setItem('minhaLista', JSON.stringify(meuApp._lista));
+
+            // atualiza a lista na pagina
+            meuApp.printTarefas();
+
+            // limpa o textinput
+            _txt.value = '';
+        } else {
+            // exibe erro ao usuario
+            alert('Escreva uma tarefa');
+        }
+    },
+
+    marcarTarefa: function(e){
+
+        // testa se foi clicado apagar tarefa
+        if (e.target.className == 'del'){
+            // remove da lista de tarefas
+            meuApp._lista.splice(e.target.dataset.id, 1);
+
+            // atualiza a lista na página
+            meuApp.printTarefas();
+        } else {
+            // altera o valor do status da tarefa na lista de tarefas
+            meuApp._lista[e.target.dataset.id].status = !meuApp._lista[e.target.dataset.id].status;
+
+            // altera a formatação do texto na tarefa
+            e.target.setAttribute('class', 'texto'+(meuApp._lista[e.target.dataset.id].status ? ' concluido':''));
+
+            // altera a cor do status da tarefa
+            document.getElementById("status-"+e.target.dataset.id).setAttribute('class', 'status '+(meuApp._lista[e.target.dataset.id].status ? 'fundo-verde':'fundo-vermelho'));
+        }
+
+        // salva na memória
+        localStorage.setItem('minhaLista', JSON.stringify(meuApp._lista));
+    }
+
+};
+
+// inicia a lista de tarefas
+meuApp.init();
