6051e0328d5cb2a4c34f41ec0b8bb388fe28b287
Walter Gandarella <walter.wgbn@gmail.com>
2015-05-13 11:05:25 -0300

    news react

--- react-build/questao-07-react-full.js
+++ react-build/questao-07-react-full.js
@@ -1,61 +1,56 @@
 var _lista = [];
 
 var ListaTarefas = React.createClass({displayName: "ListaTarefas",
+    lista: [],
+    getInitialState: function(){
+        console.info('ListaTarefas::getInitialState()');
+        return { sts: new Date().getTime() };
+    },
+
     componentDidMount: function(){
-        if (localStorage.getItem('minhaLista') != null)
-            window._lista = JSON.parse(localStorage.getItem('minhaLista'));
+        console.info('ListaTarefas::componentDidMount()');
+        if (localStorage.getItem('minhaLista') != null){
+            _lista = JSON.parse(localStorage.getItem('minhaLista'));
+            //this.setState({lista: _lista});
+            this.lista = _lista;
+            this.setState({sts: new Date().getTime()});
+        }
+    },
+
+    handleNovaTarefa: function(){
+        console.info('ListaItens::handleNovaTarefa()');
+
+        this.lista = _lista;
+        this.setState({sts: new Date().getTime()});
+        console.log(this.state.sts);
     },
 
     render: function(){
+        console.info('ListaTarefas::render()');
+        console.log(this.lista);
         return (
             React.createElement("main", {className: "geral"},
                 React.createElement("h1", null, "Lista de Tarefas"),
 
-                React.createElement(ListaForm, null),
+                React.createElement(ListaForm, {onNovaTarefa: this.handleNovaTarefa}),
 
-                React.createElement(ListaItens, {lista: window._lista})
-            )
-        );
-    }
-});
-
-var ListaForm = React.createClass({displayName: "ListaForm",
-    componentDidMount: function(){
-        this._campo = document.querySelector('#tarefa');
-    },
-
-    inserirTarefa: function(){
-        if (this._campo.value.length > 0){
-            window._lista.push({tarefa: this._campo.value, status: 0});
-            localStorage.setItem('minhaLista', JSON.stringify(window._lista));
-            this._campo.value = '';
-        } else {
-            alert('Preencha uma tarefa');
-        }
-    },
-
-    render: function(){
-        return (
-            React.createElement("section", {className: "barra clearfix"},
-                React.createElement("input", {type: "text", id: "tarefa", maxlength: "1000"}),
-                React.createElement("button", {onClick: this.inserirTarefa}, "inserir")
+                React.createElement(ListaItens, {data: this.lista, onAtualiza: this.handleNovaTarefa})
             )
         );
     }
 });
 
 var ListaItens = React.createClass({displayName: "ListaItens",
-    getInitialState: function(){
-        return { lista: this.props.lista };
-    },
-
-    componentDidMount: function(){
-        console.log(this.state.lista);
+    handleMudarEstado: function(e){
+        console.info('ListaItens::handleMudarEstado()');
+        this.props.onAtualiza();
     },
 
     render: function(){
-        var todasTarefas = this.state.lista.map(function (_val, _key) { console.log(_val);
-            return React.createElement(ItemLista, {chave: _key, tarefa: _val.tarefa, status: _val.status});
+        console.info('ListaItens::render()');
+        var self = this;
+        var todasTarefas = this.props.data.map(function (_val, _key){
+            return React.createElement(ItemLista, {chave: _key, tarefa: _val.tarefa, status: _val.status, onMudarEstado: self.handleMudarEstado});
         });
 
         return (
@@ -66,38 +61,87 @@ var ListaItens = React.createClass({displayName: "ListaItens",
     }
 });
 
-var ListaItem = React.createClass({displayName: "ListaItem",
+var ItemLista = React.createClass({displayName: "ItemLista",
+    getInitialState: function() {
+        console.info('ListaItem::getInitialState()');
+        return {status: this.props.status || false};
+    },
+
     marcarTarefa: function(e){
+        console.info('ListaItem::marcarTarefa()');
         // testa se foi clicado apagar tarefa
         if (e.target.className == 'del'){
+            console.info('ListaItem::marcarTarefa() -> del()');
             // remove da lista de tarefas
-            this.props.lista.splice(e.target.dataset.id, 1);
+            _lista.splice(e.target.dataset.id, 1);
+            // atualiza localStorage
+            localStorage.setItem('minhaLista', JSON.stringify(_lista));
+            // remove da lista
+            //document.querySelector('.lista').removeChild(document.getElementById('tarefa-'+e.target.dataset.id));
         } else {
+            console.info('ListaItem::marcarTarefa() -> altera()');
             // altera o valor do status da tarefa na lista de tarefas
-            this.props.lista[e.target.dataset.id].status = !this.props.lista[e.target.dataset.id].status;
+            _lista[e.target.dataset.id].status = !_lista[e.target.dataset.id].status;
+            _lista[e.target.dataset.id].timestamp = new Date().getTime();
+            // atualiza localStorage
+            localStorage.setItem('minhaLista', JSON.stringify(_lista));
 
             // altera a formatação do texto na tarefa
-            e.target.setAttribute('class', 'texto'+(this.props.lista[e.target.dataset.id].status ? ' concluido':''));
+            //e.target.setAttribute('class', 'texto'+(_lista[e.target.dataset.id].status ? ' concluido':''));
 
             // altera a cor do status da tarefa
-            document.getElementById("status-"+e.target.dataset.id).setAttribute('class', 'status '+(this.props.lista[e.target.dataset.id].status ? 'fundo-verde':'fundo-vermelho'));
+            //document.getElementById("status-"+e.target.dataset.id).setAttribute('class', 'status '+(_lista[e.target.dataset.id].status ? 'fundo-verde':'fundo-vermelho'));
         }
+
+        this.props.onMudarEstado();
     },
 
-    getInitialState: function() {
-        return {status: this.props.status || 0};
-    },
-
-    toggle: function() {
+    /*toggle: function() {
+        console.info('ListaItem::toggle()');
         this.setState({status: !this.state.status});
-    },
+    },*/
 
     render: function(){
+        console.info('ListaItem::render()');
+        var _status = (this.state.status ? 'fundo-verde':'fundo-vermelho');
+        var _texto = (this.state.status ? ' concluido':'');
         return (
             React.createElement("div", {className: "item", id: "tarefa-"+this.props.chave, "data-id": this.props.chave, onClick: this.marcarTarefa},
                 React.createElement("div", {className: "status "+_status, id: "status-"+this.props.chave, "data-id": this.props.chave}, " "),
-                React.createElement("div", {className: "texto ", id: "text-"+this.props.chave, "data-id": this.props.chave}, this.props.tarefa),
+                React.createElement("div", {className: "texto"+_texto, id: "text-"+this.props.chave, "data-id": this.props.chave}, this.props.tarefa),
                 React.createElement("div", {className: "del", id: "del-"+this.props.chave, "data-id": this.props.chave}, "X")
+            )
+        );
+    }
+});
+
+var ListaForm = React.createClass({displayName: "ListaForm",
+    inserirTarefa: function(){
+        console.info('ListaForm::inserirTarefa()');
+        var _campo = React.findDOMNode(this.refs.tarefa);
+
+        if (_campo.value.length > 0){
+            _lista.push({tarefa: _campo.value, status: false, timestamp: new Date().getTime()});
+            localStorage.setItem('minhaLista', JSON.stringify(window._lista));
+            _campo.value = '';
+
+            this.props.onNovaTarefa();
+        } else {
+            alert('Preencha uma tarefa');
+        }
+    },
+
+    /*handleMudar: function(){
+        console.info('ListaForm::inserirTarefa()');
+        this.props.onAtualiza();
+    },*/
+
+    render: function(){
+        console.info('ListaForm::render()');
+        return (
+            React.createElement("section", {className: "barra clearfix"},
+                React.createElement("input", {type: "text", ref: "tarefa", maxlength: "1000"}),
+                React.createElement("button", {onClick: this.inserirTarefa}, "inserir")
             )
         );
     }
--- react-src/questao-07-react-full.js
+++ react-src/questao-07-react-full.js
@@ -1,61 +1,56 @@
 var _lista = [];
 
 var ListaTarefas = React.createClass({
+    lista: [],
+    getInitialState: function(){
+        console.info('ListaTarefas::getInitialState()');
+        return { sts: new Date().getTime() };
+    },
+
     componentDidMount: function(){
-        if (localStorage.getItem('minhaLista') != null)
-            window._lista = JSON.parse(localStorage.getItem('minhaLista'));
+        console.info('ListaTarefas::componentDidMount()');
+        if (localStorage.getItem('minhaLista') != null){
+            _lista = JSON.parse(localStorage.getItem('minhaLista'));
+            //this.setState({lista: _lista});
+            this.lista = _lista;
+            this.setState({sts: new Date().getTime()});
+        }
+    },
+
+    handleNovaTarefa: function(){
+        console.info('ListaItens::handleNovaTarefa()');
+
+        this.lista = _lista;
+        this.setState({sts: new Date().getTime()});
+        console.log(this.state.sts);
     },
 
     render: function(){
+        console.info('ListaTarefas::render()');
+        console.log(this.lista);
         return (
             <main className="geral">
                 <h1>Lista de Tarefas</h1>
 
-                <ListaForm />
+                <ListaForm onNovaTarefa={this.handleNovaTarefa} />
 
-                <ListaItens lista={window._lista} />
+                <ListaItens data={this.lista} onAtualiza={this.handleNovaTarefa} />
             </main>
         );
     }
 });
 
-var ListaForm = React.createClass({
-    componentDidMount: function(){
-        this._campo = document.querySelector('#tarefa');
-    },
-
-    inserirTarefa: function(){
-        if (this._campo.value.length > 0){
-            window._lista.push({tarefa: this._campo.value, status: 0});
-            localStorage.setItem('minhaLista', JSON.stringify(window._lista));
-            this._campo.value = '';
-        } else {
-            alert('Preencha uma tarefa');
-        }
-    },
-
-    render: function(){
-        return (
-            <section className="barra clearfix">
-                <input type="text" id="tarefa" maxlength="1000" />
-                <button onClick={this.inserirTarefa}>inserir</button>
-            </section>
-        );
-    }
-});
-
 var ListaItens = React.createClass({
-    getInitialState: function(){
-        return { lista: this.props.lista };
-    },
-
-    componentDidMount: function(){
-        console.log(this.state.lista);
+    handleMudarEstado: function(e){
+        console.info('ListaItens::handleMudarEstado()');
+        this.props.onAtualiza();
     },
 
     render: function(){
-        var todasTarefas = this.state.lista.map(function (_val, _key) { console.log(_val);
-            return <ItemLista chave={_key} tarefa={_val.tarefa} status={_val.status} />;
+        console.info('ListaItens::render()');
+        var self = this;
+        var todasTarefas = this.props.data.map(function (_val, _key){
+            return <ItemLista chave={_key} tarefa={_val.tarefa} status={_val.status} onMudarEstado={self.handleMudarEstado} />;
         });
 
         return (
@@ -66,39 +61,88 @@ var ListaItens = React.createClass({
     }
 });
 
-var ListaItem = React.createClass({
+var ItemLista = React.createClass({
+    getInitialState: function() {
+        console.info('ListaItem::getInitialState()');
+        return {status: this.props.status || false};
+    },
+
     marcarTarefa: function(e){
+        console.info('ListaItem::marcarTarefa()');
         // testa se foi clicado apagar tarefa
         if (e.target.className == 'del'){
+            console.info('ListaItem::marcarTarefa() -> del()');
             // remove da lista de tarefas
-            this.props.lista.splice(e.target.dataset.id, 1);
+            _lista.splice(e.target.dataset.id, 1);
+            // atualiza localStorage
+            localStorage.setItem('minhaLista', JSON.stringify(_lista));
+            // remove da lista
+            //document.querySelector('.lista').removeChild(document.getElementById('tarefa-'+e.target.dataset.id));
         } else {
+            console.info('ListaItem::marcarTarefa() -> altera()');
             // altera o valor do status da tarefa na lista de tarefas
-            this.props.lista[e.target.dataset.id].status = !this.props.lista[e.target.dataset.id].status;
+            _lista[e.target.dataset.id].status = !_lista[e.target.dataset.id].status;
+            _lista[e.target.dataset.id].timestamp = new Date().getTime();
+            // atualiza localStorage
+            localStorage.setItem('minhaLista', JSON.stringify(_lista));
 
             // altera a formatação do texto na tarefa
-            e.target.setAttribute('class', 'texto'+(this.props.lista[e.target.dataset.id].status ? ' concluido':''));
+            //e.target.setAttribute('class', 'texto'+(_lista[e.target.dataset.id].status ? ' concluido':''));
 
             // altera a cor do status da tarefa
-            document.getElementById("status-"+e.target.dataset.id).setAttribute('class', 'status '+(this.props.lista[e.target.dataset.id].status ? 'fundo-verde':'fundo-vermelho'));
+            //document.getElementById("status-"+e.target.dataset.id).setAttribute('class', 'status '+(_lista[e.target.dataset.id].status ? 'fundo-verde':'fundo-vermelho'));
         }
+
+        this.props.onMudarEstado();
     },
 
-    getInitialState: function() {
-        return {status: this.props.status || 0};
-    },
-
-    toggle: function() {
+    /*toggle: function() {
+        console.info('ListaItem::toggle()');
         this.setState({status: !this.state.status});
-    },
+    },*/
 
     render: function(){
+        console.info('ListaItem::render()');
+        var _status = (this.state.status ? 'fundo-verde':'fundo-vermelho');
+        var _texto = (this.state.status ? ' concluido':'');
         return (
             <div className="item" id={"tarefa-"+this.props.chave} data-id={this.props.chave} onClick={this.marcarTarefa}>
                 <div className={"status "+_status} id={"status-"+this.props.chave} data-id={this.props.chave}>&nbsp;</div>
-                <div className="texto " id={"text-"+this.props.chave}  data-id={this.props.chave}>{this.props.tarefa}</div>
+                <div className={"texto"+_texto} id={"text-"+this.props.chave}  data-id={this.props.chave}>{this.props.tarefa}</div>
                 <div className="del" id={"del-"+this.props.chave} data-id={this.props.chave}>X</div>
             </div>
+        );
+    }
+});
+
+var ListaForm = React.createClass({
+    inserirTarefa: function(){
+        console.info('ListaForm::inserirTarefa()');
+        var _campo = React.findDOMNode(this.refs.tarefa);
+
+        if (_campo.value.length > 0){
+            _lista.push({tarefa: _campo.value, status: false, timestamp: new Date().getTime()});
+            localStorage.setItem('minhaLista', JSON.stringify(window._lista));
+            _campo.value = '';
+
+            this.props.onNovaTarefa();
+        } else {
+            alert('Preencha uma tarefa');
+        }
+    },
+
+    /*handleMudar: function(){
+        console.info('ListaForm::inserirTarefa()');
+        this.props.onAtualiza();
+    },*/
+
+    render: function(){
+        console.info('ListaForm::render()');
+        return (
+            <section className="barra clearfix">
+                <input type="text" ref="tarefa" maxlength="1000" />
+                <button onClick={this.inserirTarefa}>inserir</button>
+            </section>
         );
     }
 });
